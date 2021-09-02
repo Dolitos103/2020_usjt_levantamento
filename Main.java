d1f53aa5a4d31feffa4d86d8e6e54646063a2fd1
Douglas Oliveira <56274144+Dolitos103@users.noreply.github.com>
2021-09-02 00:43:48 +0000

    Alterações Aula 2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Lista{
   int [] elementos;
   int qtde;
@@ -9,19 +11,52 @@ class Lista{
   }
 
   boolean estaCheia(){
-    return qtde == cap;
+    return qtde >= cap;
     //return qtde == cap ? true:false;
+  }
+
+  public void setQtde(int qtde){
+    this.qtde = qtde;
+  }
+
+  void aumentar (){
+    //alocar um novo vetor com o dobro de tamanho
+    int [] aux = new int[this.cap*2];
+    for(int i = 0; i < this.qtde; i++){
+      aux [i] = elementos[i];
+    }
+    //dobra a capacidade
+    this.cap *=2;
+    //ajustar a variavel de referência
+    elementos = aux;
   }
   
   void inserir (int e) {
+    if(estaCheia())
+      aumentar();
     elementos[qtde] = e;
     qtde++;
   }
-}
 
+  void exibir (){
+    System.out.printf("***************\n");
+    System.out.printf("Qtde: %d\n", qtde);
+    System.out.printf("cap: %d\n", cap);
+    for(int i = 0; i < this.qtde; i++){
+      System.out.printf("%d ", this.elementos[i]);
+    }
+    System.out.printf("\n");
+  }
+}
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Random gerador = new Random();
+    Lista umaLista = new Lista();
+    for(int i = 0; i < 100; i++){
+      int dado = gerador.nextInt(20) + 1;
+      umaLista.inserir(dado);
+      umaLista.exibir();
+    }
   }
 }
